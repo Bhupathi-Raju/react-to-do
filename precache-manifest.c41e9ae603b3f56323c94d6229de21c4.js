4beb1601ebea2e86e18bae738e04af39bded9261
Bhupathi2069 <raj@143hari>
2020-06-25 12:20:23 +0530

    Updates

---
+++ precache-manifest.c41e9ae603b3f56323c94d6229de21c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5072d5d8e91d0b47c686def348520b4",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "99ace19a8c730d313891",
+    "url": "/react-to-do/static/css/main.713a6b05.chunk.css"
+  },
+  {
+    "revision": "b24905bd96ff882a20a4",
+    "url": "/react-to-do/static/js/2.0f2fbd06.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-to-do/static/js/2.0f2fbd06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ace19a8c730d313891",
+    "url": "/react-to-do/static/js/main.74a63a88.chunk.js"
+  },
+  {
+    "revision": "6f00fde01e7e9560888f",
+    "url": "/react-to-do/static/js/runtime-main.094738d5.js"
+  }
+]);
